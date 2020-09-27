149802a34cefaf4b09ddc158bf857a6c3be400ca
zannex <49724296+zannex@users.noreply.github.com>
2020-09-27 23:30:07 +0200

    Add support for more picture formats

--- cut_empty.py
+++ cut_empty.py
@@ -2,16 +2,22 @@ import glob, os, argparse
 
 def delete_empty():
     num_delete = 0
+    pic_formats = ['.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.tif', '.webp']
         #Train
     for f in glob.glob(r'data/coco/labels/train/**/*.txt', recursive=True):
-                fpath, fname = os.path.split(f)
-                rname, extname = os.path.splitext(fname)
-                dirtup = (f, rname)
-                if os.stat(str(f)).st_size == 0:
-                        os.remove(str(f))
-                        os.remove((str(f).replace('data/coco/labels/','data/coco/images/')).replace('.txt','.jpg'))
-              
-                    
+        fpath, fname = os.path.split(f)
+        rname, extname = os.path.splitext(fname)
+        dirtup = (f, rname)
+        if os.stat(str(f)).st_size == 0:
+            for pic_format in pic_formats:
+                pic = (str(f).replace('data/coco/labels/','data/coco/images/')).replace('.txt',pic_format)
+                try:
+                    os.remove(str(pic))
+                    os.remove(str(f))
+                    num_delete+=1
+                    break    
+                except:
+                    pass                                
         
     print("Finished! Found: ", num_delete, " empty labels")
 
