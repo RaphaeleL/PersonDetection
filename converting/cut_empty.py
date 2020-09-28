71fbf10bfe48d561e144df5a4c8f13b37df8ba7d
zannex <49724296+zannex@users.noreply.github.com>
2020-09-28 16:58:30 +0200

    Add 'delete train pictures without label'

--- converting/cut_empty.py
+++ converting/cut_empty.py
@@ -3,21 +3,30 @@ import glob, os, argparse
 def delete_empty():
     num_delete = 0
     pic_formats = ['.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.tif', '.webp']
-        #Train
+    #Delete train pictures with empty labels
     for f in glob.glob(r'../data/coco/labels/train/**/*.txt', recursive=True):
         fpath, fname = os.path.split(f)
         rname, extname = os.path.splitext(fname)
         dirtup = (f, rname)
-        if os.stat(str(f)).st_size == 0:
+        if os.stat(str(f)).st_size == 0 or os.path.getsize(str(f)) == 0:
             for pic_format in pic_formats:
                 pic = (str(f).replace('../data/coco/labels/','../data/coco/images/')).replace('.txt',pic_format)
                 try:
-                    os.remove(str(pic))
+                    os.remove(pic)
                     os.remove(str(f))
                     num_delete+=1
                     break    
                 except:
-                    pass                                
+                    pass
+    #Delete train pictures without labels            
+    for pic_format in pic_formats:
+        for f in glob.glob(r'data/coco/images/train/**/*'+pic_format, recursive=True):
+            label = str(f).replace('data/coco/images/','data/coco/labels/').replace(pic_format,".txt")
+            try: 
+              cache = os.path.getsize(label) 
+            except:
+              os.remove(f);
+              num_delete+=1
         
     print("Finished! Found: ", num_delete, " empty labels")
 
